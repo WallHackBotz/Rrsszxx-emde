17abb4504c2885f17eb68a442f948d2a467ec35f
Rrsszxx <96643671+WallHackBotz@users.noreply.github.com>
2022-04-17 06:16:00 +0700

    Add files via upload

--- settings.js
+++ settings.js
@@ -3,36 +3,36 @@ const chalk = require('chalk')
 
 // Website Api
 global.APIs = {
-	alfa: 'https://api-alphabot.herokuapp.com',
+	alfa: 'https://rrsszxx-api.herokuapp.com',
 }
 
 // Free apikey
 global.APIKeys = {
-	'https://api-alphabot.herokuapp.com': 'Alphabot',
+	'https://rrsszxx-api.herokuapp.com': 'Rrsszxx',
 }
 
 // setting 
-global.autoread = false // auto read pesan / message
+global.autoread = true // auto read pesan / message
 global.autorecording = true //status auto merekam ( auto record )
-global.autoketik = false //status auto mengetik (auto typing)
-global.available = false //status online (online)
+global.autoketik = true //status auto mengetik (auto typing)
+global.available = true //status online (online)
 
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb ="https://api-alphabot.herokuapp.com/" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
-global.github = "https://zeeone-ofc.github.io/" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.botname = "Rrsszxx-Mdོ" //namabot kalian
+global.ownername= "ᴹᴿ᭄ Rrsszxx ×፝֟͜×" //nama kalian
+global.myweb ="https://rrsszxx-api.herokuapp.com/" //bebas asal jan hapus
+global.youtube = "https://youtube.com/c/Rrsszxx Official" //bebas asal jan hapus
+global.github = "https://rrsszxx98.github.io/" //bebas
+global.email = "rrsszxx98@gmail.com" //bebas
 global.region = "Indonesia" //bebas
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.ownernomer = "62895328255152" // nomor wa kalian
+global.ownernomerr = "+62895328255152" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["62887435047326","62887435047326","6285342106390"] //ganti agar fitur owner bisa di gunakan
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.owner = ["62895328255152","62895328255152","62895401928948"] //ganti agar fitur owner bisa di gunakan
+global.packname = '© Rrsszxx-Mdོ' //sticker wm ubah
+global.author = 'Di Buat Oleh Rrsszxx' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
